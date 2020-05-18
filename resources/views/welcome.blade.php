320c6a5714360fa667766aff4c286f7f76222452
Davy Biron <sfu1982@gmail.com>
2020-05-18 14:41:50 +0200

    test changement titre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    test
+                    Cool sa fonctionne !!!
                 </div>
 
                 <div class="links">
