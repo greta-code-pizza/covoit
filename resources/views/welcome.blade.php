b629c8a54191ba46962269cdd3cf5366542dfba1
Gaylor Marditsas <gaylor273@outlook.fr>
2020-05-18 09:26:09 +0200

    test auto-deploy on heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    It's WORKING !
                 </div>
 
                 <div class="links">
