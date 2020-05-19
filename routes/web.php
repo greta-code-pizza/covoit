c63a94c0e97cd1f19853de0613f4b0f053ad95d1
Gaylor Marditsas <gaylor273@outlook.fr>
2020-05-19 15:45:28 +0200

    changement route view

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', 'TokenController@generateToken');
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('contact', 'ContactController@show');
 Route::Post('contact', 'ContactController@store');
