5535c4e30c73ede872b8bbbaf5fd897de2f54c7b
unknown <albanhusar@gmail.com>
2020-05-26 09:37:41 +0200

    salut

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Cool sa fonctionne !!!
+                    Cool ça fonctionne !!!
                 </div>
 
                 <div class="links">
